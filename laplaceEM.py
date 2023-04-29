2889522aa9c3bb799de46143f1557c31a71e3ad6
yyt_saber <yyt20080808@gmail.com>
2023-04-29 18:09:57 +0800

    Update laplaceEM.py
    适用于graph 中，用Laplace 计算 Δf=1 时的average degree，同时得到weights

--- laplaceEM.py
+++ laplaceEM.py
@@ -1,88 +1,96 @@
-
 import numpy as np
 from scipy.stats import laplace
 import math
-# 定义Laplace分布的数目
-num_laplace = 200 # set the number of laplace distribution
-n = 3000  # number of noised reports
-from scipy.optimize import minimize
-np.random.seed(123)
+# Generate some sample data from a Laplace mixture model
+n = 3000
+num_laplace = 21
+means =  np.linspace(0, 20, num_laplace)
+# proportions = np.array([0.2, 0.3, 0.5])
+# z = np.random.choice(len(loc), size=n, p=proportions)
+# data = np.concatenate([
+#     laplace.rvs(loc=mean, scale=2/epsilon, size=int(n/num_laplace))
+#     for mean in means
+# ])
+
 def generate_data():
     res = []
-    a = np.random.normal(0.5, math.sqrt(0.02), 1000)
+    a = np.random.normal(5, math.sqrt(0.02), 1000)
+    # a = np.random.uniform(-1, 1, 100)
     res.extend(a)
-    b = np.random.normal(0.4, math.sqrt(0.01), 1000)
+    b = np.random.normal(4, math.sqrt(0.01), 1000)
     res.extend(b)
-    c = np.random.normal(-0.1, math.sqrt(0.002), 1000)
+    c = np.random.normal(9, math.sqrt(0.2), 1000)
     res.extend(c)
-    new_res = []
-    for i in res:
-        if i <= 1 and i >= -1:
-            new_res.append(i)
-    print("mean:",sum(new_res)/len(new_res))
-    return new_res
-
+    print("mean:",sum(res)/len(res))
+    return res
 
 def laplaceMechanism(a,epsilon_list):
+    exper = 10
     res_directsum = []
     res_EM = []
     for epsilon in epsilon_list:
         accuracy = 0
         accuracy_EM = 0
-        for times in range(5):
-            print(epsilon,times)
-            res=0
+        for times in range(exper):
             all = sum(a)
+            print(epsilon,times,all)
+            res = 0
             noised_reports = []
             for j in a:
-                noise = np.random.laplace(0, 2 / epsilon, 1)
+                noise = np.random.laplace(0, 1/epsilon, 1)
                 res += (j + noise[0])
                 noised_reports.append(j+noise[0])
             length = len(a)
             accuracy += (all / length - res / length)**2
-            result = minimize(
-                lambda x: -log_likelihood(epsilon,x,noised_reports),
-                x0=np.ones(num_laplace) / num_laplace,
-                method='SLSQP',
-                bounds=[(0, 1) for i in range(num_laplace)],
-                constraints=[{'type': 'eq', 'fun': lambda x: np.sum(x) - 1}]
-            )
-            Em_est = calmean(result.x)
-            accuracy_EM += (Em_est - res / length)**2
+
+            noised_reports = np.array(noised_reports)
+            result = EM(noised_reports,epsilon)
+            print(result)
+            Em_est = calmean(result)
+            print(res / length, Em_est)
+            accuracy_EM += (Em_est - all / length)**2
             # 开始 EM
-        res_directsum.append(accuracy / 5)
-        res_EM.append(accuracy_EM/5)
+        res_directsum.append(accuracy / exper)
+        res_EM.append(accuracy_EM/exper)
     return res_directsum,res_EM
 
-# 生成样本数据
+def EM(data,eps):
+    weights = np.ones(num_laplace) / num_laplace
+    # Run EM algorithm
+    tolerance = 1e-6
+    max_iter = 1000
+    ll_old = -np.inf
+    for iter in range(max_iter):
+        # E-step: compute expected values of latent variable z
+        probs = np.zeros((n, num_laplace))
+        for j in range(num_laplace):
+            probs[:, j] = weights[j] * np.exp(-np.abs(data - means[j]) / (1/eps))
+        probs /= probs.sum(axis=1, keepdims=True)
 
-# 定义混合模型的似然函数
-def log_likelihood(epsilon,weights,data):
-    # weights: 每个Laplace分布的权重
-    means = np.linspace(-1, 1, num_laplace)
-    if np.any(weights < 0) or np.any(weights > 1):
-        return -np.inf
-    else:
-        mixture = np.sum([
-            weights[i] * laplace.logpdf(data, loc=means[i], scale=2/epsilon)
-            for i in range(num_laplace)
-        ], axis=0)
-        return np.sum(mixture)
+        # M-step: update mixture weights
+        weights = probs.mean(axis=0)
 
+        # Compute log-likelihood and check for convergence
+        ll = np.sum(np.log(probs @ weights))
+        if ll - ll_old < tolerance:
+            break
+        ll_old = ll
+    return weights
 
 def calmean(res):
+    print(res)
     normalized_arr = res / np.sum(res)
     mean_est = 0
     for i in range(num_laplace):
-        mean_est += normalized_arr[i] * (i + 0.5)
-    sum_OPMnos = (mean_est / num_laplace - 0.5) * 2  # 转换到 -1 到 1 的区间
-    return sum_OPMnos
-
+        mean_est += normalized_arr[i] * (i)
+    # mean_est = (mean_est / num_laplace - 0.5) * 2  # 转换到 -1 到 1 的区间
+    return mean_est
 
 if __name__ == "__main__":
     a =generate_data()
-    ep_list = [0.5,0.75,1]
-    res_direct, res_EM= laplaceMechanism(a,ep_list)
+    ep_list = [1]
+    res_direct, res_EM = laplaceMechanism(a, ep_list)
+# Print estimated mixture weights
     print("样本均值结果:")
     print(res_direct)
     print("混合laplace分布计算结果:")
